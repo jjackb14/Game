fb7b75541271da7fad3a20f6e907844f5ebfb9a6
jacks <jack.benoit14@gmail.com>
2025-02-21 20:06:15 -0500

    Added main

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Game");
+
+        window.setLocationRelativeTo(null);
+
+        window.setVisible(true);
+    }
+
+}
